335aa0150afb5e269be7b3d56359893346cf52c1
abdelrahman <rain.forest5023@gmail.com>
2025-09-25 01:50:50 +0300

    integrate DarkBackgroundFrame widget into SeriesDetailsPage

--- lib/presentation/ui/pages/series_details_page.dart
+++ lib/presentation/ui/pages/series_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:animyoji/gen/assets.gen.dart';
 import 'package:animyoji/helpers/extensions/int_extensions.dart';
 import 'package:animyoji/helpers/extensions/padding_extensions.dart';
+import 'package:animyoji/presentation/ui/widgets/background_frame.dart';
 import 'package:animyoji/presentation/ui/widgets/category_chip.dart';
 import 'package:animyoji/presentation/ui/widgets/series_card.dart';
 import 'package:animyoji/presentation/ui/widgets/series_content_describtion.dart';
@@ -16,106 +17,107 @@ class SeriesDetailsPage extends StatelessWidget {
   final SeriesModel series;
 
   const SeriesDetailsPage({super.key, required this.series});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
+    return DarkBackgroundFrame(
+      child: Scaffold(
         backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => Navigator.pop(context),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () => Navigator.pop(context),
+          ),
         ),
-      ),
-      body: CustomScrollView(
-        slivers: [
-          SliverStack(
-            children: [
-              SliverAppBar(
-                expandedHeight: 500.0,
-                pinned: false,
-                backgroundColor: Colors.transparent,
-                automaticallyImplyLeading: false,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Image.asset(series.cover, fit: BoxFit.cover),
-                ),
-              ),
-              if(series.seriesLogo.isNotEmpty)
-              SliverPositioned(
-                top: 550,
-                left: 0,
-                right: 0,
-                child: Center(
-                  child: SizedBox(
-                    height: 120,
-                    child: Image.asset(
-                      series.seriesLogo,
-                      height: 80,
-                    ),
+        body: CustomScrollView(
+          slivers: [
+            SliverStack(
+              children: [
+                SliverAppBar(
+                  expandedHeight: 500.0,
+                  pinned: false,
+                  backgroundColor: Colors.transparent,
+                  automaticallyImplyLeading: false,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Image.asset(series.cover, fit: BoxFit.cover),
                   ),
                 ),
-              ),
-            ],
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                if(series.seriesLogo.isNotEmpty)
-                70.height,
-                16.height,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Wrap(
-                      spacing: 8,
-                      runSpacing: 4,
-                      children: series.seriesCategory
-                          .map(
-                            (cat) => CategoryChip(label: cat.name.capitalize()),
-                      )
-                          .toList(),
+                if (series.seriesLogo.isNotEmpty)
+                  SliverPositioned(
+                    top: 550,
+                    left: 0,
+                    right: 0,
+                    child: Center(
+                      child: SizedBox(
+                        height: 120,
+                        child: Image.asset(series.seriesLogo, height: 80),
+                      ),
                     ),
-                  ],
-                ),
-                16.height,
-                StatisticsSection(statistics: series.statistics),
-                16.height,
-                SeriesContentDescribtion(series: series),
+                  ),
               ],
-            ).paddingHorizontal(20),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Theme.of(context).scaffoldBackgroundColor,
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  if (series.seriesLogo.isNotEmpty) 70.height,
+                  16.height,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Wrap(
+                        spacing: 8,
+                        runSpacing: 4,
+                        children: series.seriesCategory
+                            .map(
+                              (cat) =>
+                                  CategoryChip(label: cat.name.capitalize()),
+                            )
+                            .toList(),
+                      ),
+                    ],
+                  ),
+                  16.height,
+                  StatisticsSection(statistics: series.statistics),
+                  16.height,
+                  SeriesContentDescribtion(series: series),
+                ],
+              ).paddingHorizontal(20),
+            ),
+          ],
         ),
-        child: SafeArea(
-          child: Row(
-            children: [
-              Expanded(
-                child: CustomButton(
-                  label: "Preview",
-                  icon: Icons.timer,
-                  backgroundColor: Theme.of(context).hintColor,
-                  textColor: Theme.of(context).colorScheme.surface,
-                  borderRadius: 32,
-                  elevation: 0,
-                  onPressed: () {},
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Theme.of(context).scaffoldBackgroundColor,
+          ),
+          child: SafeArea(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CustomButton(
+                    label: "Preview",
+                    icon: Icons.timer,
+                    backgroundColor: Theme.of(context).hintColor,
+                    textColor: Theme.of(context).colorScheme.surface,
+                    borderRadius: 32,
+                    elevation: 0,
+                    onPressed: () {},
+                  ),
                 ),
-              ),
-              const SizedBox(width: 12),
-              Expanded(
-                child: CustomButton(
-                  label: "Watch Now",
-                  icon: Icons.remove_red_eye,
-                  borderRadius: 32,
-                  onPressed: () {},
+                const SizedBox(width: 12),
+                Expanded(
+                  child: CustomButton(
+                    label: "Watch Now",
+                    icon: Icons.remove_red_eye,
+                    borderRadius: 32,
+                    onPressed: () {},
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
